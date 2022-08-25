f7ff5512d4b80411769ec79f116ab4b6e7d53aff
genraven1 <genraven1@gmail.com>
2022-08-25 15:00:35 -0500

    Update data generators for buckets

--- src/main/java/com/genraven1/vampire_raven/item/ModItemTags.java
+++ src/main/java/com/genraven1/vampire_raven/item/ModItemTags.java
@@ -9,4 +9,5 @@ public final class ModItemTags {
     public static final TagKey<Item> RAW_GEMSTONES = TagKey.create(Registry.ITEM_REGISTRY, RavenUtils.getResourceLocation("raw_gemstones"));
     public static final TagKey<Item> GEMSTONES = TagKey.create(Registry.ITEM_REGISTRY, RavenUtils.getResourceLocation("gemstones"));
     public static final TagKey<Item> KNIVES = TagKey.create(Registry.ITEM_REGISTRY, RavenUtils.getResourceLocation("knives"));
+    public static final TagKey<Item> BLOOD_BUCKETS = TagKey.create(Registry.ITEM_REGISTRY, RavenUtils.getResourceLocation("blood_buckets"));
 }
